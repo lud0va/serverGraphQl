1394ead990b7e0dba71468ca4ff9a25f5325c5c8
lud0va <ovando.decena.luis@gmail.com>
2024-02-09 09:34:02 +0100

    s

---
+++ src/main/java/com/example/servergraphql/ServerGraphQlApplication.java
@@ -0,0 +1,13 @@
+package com.example.servergraphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerGraphQlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerGraphQlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/servergraphql/ServerGraphQlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.servergraphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerGraphQlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

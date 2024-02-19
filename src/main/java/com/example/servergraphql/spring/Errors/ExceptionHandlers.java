20598dc74e68d496e5c17cb209967813a75026f1
lud0va <ovando.decena.luis@gmail.com>
2024-02-19 11:10:07 +0100

    ZA

--- src/main/java/com/example/servergraphql/spring/Errors/ExceptionHandlers.java
+++
@@ -1,7 +0,0 @@
-package com.example.servergraphql.spring.Errors;
-
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-@RestControllerAdvice
-public class ExceptionHandlers {
-}
